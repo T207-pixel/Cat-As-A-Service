deeefebee4533a0542de8d31fd3a78ebb0e14a11
T207-pixel <timnov2002@gmail.com>
2023-08-14 22:17:55 +0300

    add fragmentation to UDPSERVER

--- UDPClient.java
+++ UDPClient.java
@@ -8,10 +8,67 @@ public class UDPClient {
     private DatagramSocket datagramSocket;  // socket for communication
     private InetAddress inetAddress;    // IP address of server that we're going to be sending packet
     private byte[] buffer;  // info that's going to be sent
+    private final int size = 4; // size of datagram
 
     public UDPClient(DatagramSocket datagramSocket, InetAddress inetAddress) {
         this.datagramSocket = datagramSocket;
         this.inetAddress = inetAddress;
+    }
+
+    private String[] messageConverter(String origMsg){
+        double linesTmp = Math.ceil(origMsg.length() / (float)size);
+        int lines = (int)linesTmp;
+        String[] msgArr = new String[lines];
+        int position = 0;
+        while (origMsg.length() >= size) {
+            StringBuilder part = new StringBuilder();
+            part.append(position);
+            part.append("|");
+            part.append(origMsg.substring(0, size));
+            origMsg = origMsg.substring(size);
+            msgArr[position] = String.valueOf(part);
+            position++;
+        }
+        if (origMsg.length() > 0){
+            int dif = size - origMsg.length();
+            StringBuilder part = new StringBuilder();
+            part.append(position);
+            part.append("|");
+            part.append(origMsg);
+            for (int i = 0; i < dif; i++)
+                part.append("&");
+            msgArr[position] = String.valueOf(part);
+        }
+        return msgArr;
+    }
+
+    private void sendParts(String[] partsArr) throws IOException {
+        for (String part : partsArr){
+            buffer = part.getBytes();
+            DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length, inetAddress, 1234); // inetAddress - is where we want to send it to, port is hardcoded have to change
+            datagramSocket.send(datagramPacket);
+        }
+        buffer = "".getBytes();
+    }
+
+    private void receiveResponse() throws IOException { //тут застряли, нужно отсторитровать сообщения, обработать приходящие строки и выывести на экран нотрмально
+        String resMsg = "";
+        while(true) {
+            //удалить первые 2 символа
+            String str = "000000000000";
+            buffer = str.getBytes();
+            //byte[] buffer = new byte[512];
+            DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+            datagramSocket.receive(response);
+            String messageFromServer = new String(response.getData(), 0, response.getLength());
+            messageFromServer = messageFromServer.substring(2);
+            resMsg = resMsg + messageFromServer;
+            if (resMsg.indexOf('&') != -1){
+                resMsg = resMsg.replaceAll("&", "");
+                break;
+            }
+        }
+        System.out.println(resMsg);
     }
 
     public void sendThenReceive() {
@@ -22,19 +79,23 @@ public class UDPClient {
                         1) Go back to menu -> Enter "END"
                         2) Enter your nickname and dish in format "@Name - Dish~\"""");
                 String inputString = scanner.nextLine();
-                ////////////////////////////////////////////---SEND-REQUEST---/////////////////////////////////////////
-                buffer = inputString.getBytes();
-                DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length, inetAddress, 1234); // inetAddress - is where we want to send it to, port is hardcoded have to change
-                datagramSocket.send(datagramPacket);
-                ////////////////////////////////////////---RECEIVE-RESPONSE---//////////////////////////////////////////
-                byte[] buffer = new byte[512];  // allocate bytes for response message
-                DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-                datagramSocket.receive(response);
-                String messageFromServer = new String(response.getData(), 0, response.getLength());
-                System.out.println(messageFromServer);
                 if (inputString.equals("END")){
                     break;
                 }
+                String[] msgArr = messageConverter(inputString);
+                sendParts(msgArr);
+                ////////////////////////////////////////////---SEND-REQUEST---/////////////////////////////////////////
+//                buffer = inputString.getBytes(); // уже тут нужно передавать разделенные кусочки
+//                DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length, inetAddress, 1234); // inetAddress - is where we want to send it to, port is hardcoded have to change
+//                datagramSocket.send(datagramPacket);
+                ////////////////////////////////////////---RECEIVE-RESPONSE---//////////////////////////////////////////
+
+                receiveResponse();
+                //byte[] buffer = new byte[512];  // allocate bytes for response message
+                //DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+                //datagramSocket.receive(response);
+                //String messageFromServer = new String(response.getData(), 0, response.getLength());
+                //System.out.println(messageFromServer);
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -46,7 +107,7 @@ public class UDPClient {
     public static void main() throws SocketException, UnknownHostException {
         DatagramSocket datagramSocket = new DatagramSocket();
         InetAddress inetAddress = InetAddress.getByName("5.42.220.74"); // server IP destination address
-        UDPClient client = new UDPClient(datagramSocket, inetAddress);
+        UDPClient client = new UDPClient(datagramSocket, inetAddress); // PORT + IPADDRESS/ порядок_сообщения_id/содержание сообщения
         client.sendThenReceive();
     }
 
--- UDPServer.java
+++ UDPServer.java
@@ -6,14 +6,20 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.sql.SQLException;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class UDPServer {
     private final DatagramSocket datagramSocket;
-    private byte[] buffer = new byte[256];    //messages which is sent from client are stored in this buffer
+    private InetAddress inetAddress;
+    private int port;
+    private byte[] buffer = new byte[12];    //messages which is sent from client are stored in this buffer
+    private List<Map<Integer, String>> listOfMaps = new ArrayList<>();
+    private Map<Integer, Integer> additionalList = new HashMap<>();
+    private StringBuilder formedMsg = new StringBuilder();
+    private int id;
 
     public UDPServer(DatagramSocket datagramSocket) {
         this.datagramSocket = datagramSocket;
@@ -36,56 +42,220 @@ public class UDPServer {
         if (dishPresents) {
             DatabaseLink.addRecordInFeedList(userName, offeredDish);
             System.out.println("Dish present in cats menu");
-            return "~ Eaten by the Cat";
+            return "~ Eaten by the Cat&";
         }
         else {
             System.out.println("Entered dish is absent");
-            return "~ The Cat is amused by #" + offeredDish + "\n";
+            return "~ The Cat is amused by #" + offeredDish + "&" + "\n";
         }
 
     }
 
+    private void receivedMessages() throws IOException, SQLException {  //вероятно придется использовать отдельный поток
+        //while(true) {
+            String str = "000000000000";
+            buffer = str.getBytes();
+            DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
+            datagramSocket.receive(datagramPacket);
+            inetAddress = datagramPacket.getAddress();
+            port = datagramPacket.getPort();
+            String address = inetAddress.toString().replaceAll("^.", "").replaceAll("\\.", "");
+            id = datagramPacket.getPort() + Integer.parseInt(address);
+            String recStrPart = new String(datagramPacket.getData(), datagramPacket.getOffset(), datagramPacket.getLength()) + "|" + id;
+            String[] part = divideStrOnParts(recStrPart);
+            insertInMatrix(part); //тут ломается
+            if (ifTilda()){
+                stingProcessing();
+            }
+            System.out.println(listOfMaps);
+        //}
+
+    }
+
+    private String[] divideStrOnParts(String str){ //работает неверно
+        String[] outputStr = new String[3];
+        for (int i = 0; i < 2; i++){
+            int present = str.indexOf('|');
+            if (present != -1){
+                outputStr[i] = str.substring(0, present);
+                str = str.substring(present + 1);
+            }
+        }
+        outputStr[2] = str;
+        return outputStr;
+    }
+
+    private void insertInMatrix(String[] part){
+        int place = Integer.parseInt(part[0]);
+        String textPart = part[1];
+        int id = Integer.parseInt(part[2]);;
+        Map<Integer, String> fragment = new HashMap<Integer, String>();
+        int arrPlace = -1;
+        fragment.put(place, textPart);
+        for (Map.Entry<Integer, Integer> entry : additionalList.entrySet()) {
+            if (entry.getValue().equals(id)) {
+                arrPlace = entry.getKey();
+            }
+        }
+        if (arrPlace >= 0){
+            listOfMaps.get(arrPlace).put(place, textPart);
+        } else {
+            additionalList.put(listOfMaps.size(), id);
+            listOfMaps.add(fragment);
+            arrPlace = 0;
+        }
+    }
+
+    private boolean ifTilda(){
+        String str = "";
+        for (int i = 0; i < listOfMaps.size(); i++) {
+            for (int j = 1; j <= listOfMaps.get(i).size(); j++) {
+                Object obj = listOfMaps.get(i).get(j);
+                if (obj != null){
+                    str = obj.toString();
+                    if (str.indexOf('~') != -1){
+                        if (checkLength(i)){
+                            return true;
+                        }
+                    }
+
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checkLength(int mapIndex){
+        int testCounter = 0;
+        //for (int i = 0; i < listOfMaps.size(); i++){
+            for (int j = 0; j < listOfMaps.get(mapIndex).size(); j++){
+                if (listOfMaps.get(mapIndex).containsKey(j))
+                    testCounter++;
+            }
+            if (testCounter == listOfMaps.get(mapIndex).size()){
+                createMsg(mapIndex);  //NEW
+                return true;
+            }
+        return false;
+    }
+
+    private void createMsg(int mapIndex){  //NEW
+        for (int i = 0; i < listOfMaps.get(mapIndex).size(); i++){
+            String str = listOfMaps.get(mapIndex).get(i);
+            formedMsg.append(str);
+        }
+        listOfMaps.remove(listOfMaps.get(mapIndex));
+        additionalList.remove(mapIndex);
+    }
+
+    private void stingProcessing() throws SQLException, IOException { //NEW
+        String messageFromClient = formedMsg.toString();
+        messageFromClient = messageFromClient.replaceAll("&", "");
+        String msg;
+        if (messageFromClient.equals("END")){
+            msg = "Current session is stopped";
+            System.out.println("END");
+            //break;
+        } else {
+            messageFromClient = messageFromClient.replaceAll(".$", "");
+            String clientName = UDPServer.getClientInputParams(messageFromClient, "\\b\\w+");
+            String clientDish = UDPServer.getClientInputParams(messageFromClient, "\\b\\w+$");
+            System.out.print("Message from client: " + messageFromClient + "\nClient name: " +
+                    clientName + "\nClient dish: " + clientDish + "\n");
+            msg = UDPServer.dishPresentsInMenu(clientName, clientDish);
+            formedMsg.delete(0, formedMsg.length());
+            formedMsg.append(msg);
+            sendMessage();
+            // дальше какой-то код с буффером 3 строки нужно опять разбить сообщение от сервера на нужные куски и отправить
+        }
+    }
+
+    private void sendMessage() throws IOException {
+        if (!formedMsg.isEmpty()){
+            sendParts(messageConverter());
+        }
+    }
+
+    private String[] messageConverter(){
+        int size = buffer.length - 3;
+        String sendingMsg = String.valueOf(formedMsg);
+        double linesTmp = Math.ceil(sendingMsg.length() / (float)size);
+        int lines = (int)linesTmp;
+        String[] msgArr = new String[lines];
+        int position = 0;
+        while (sendingMsg.length() >= size) {
+            StringBuilder part = new StringBuilder();
+            part.append(position);
+            part.append("|");
+            part.append(sendingMsg.substring(0, size));
+            sendingMsg = sendingMsg.substring(size);
+            msgArr[position] = String.valueOf(part);
+            position++;
+        }
+        if (sendingMsg.length() > 0){
+            int dif = size - sendingMsg.length();
+            StringBuilder part = new StringBuilder();
+            part.append(position);
+            part.append("|");
+            part.append(sendingMsg);
+            for (int i = 0; i < dif; i++)
+                part.append("&");
+            msgArr[position] = String.valueOf(part);
+        }
+        return msgArr;
+    }
+
+    private void sendParts(String[] partsArr) throws IOException {
+        //DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
+        //InetAddress inetAddress = datagramPacket.getAddress();
+        //int port = datagramPacket.getPort();
+        for (String part : partsArr){
+            buffer = part.getBytes();
+            DatagramPacket response = new DatagramPacket(buffer, buffer.length, inetAddress, port);
+            datagramSocket.send(response);
+        }
+        buffer = "".getBytes();
+        formedMsg.setLength(0);
+    }
+
+
     public void receiveThenSend() {
         while(true) {
             try {
-                DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);  // implements a connectionless packet delivery service (no connection between client and server,
-                                                                                            // each packet sent or received on a datagram socket is individually addressed and routed
-                                                                                            // buffer - holds data that receives from client. buffer.length - quantity of symbols that should be read from a datagram
-                datagramSocket.receive(datagramPacket); // blocking method, program will be halted here until this method returns
-                InetAddress inetAddress = datagramPacket.getAddress();      //IP information that it came from
-                                                                            // that IP is used to send a datagram back to the client
-                int port = datagramPacket.getPort(); //we need a port number that the packet was sent over
-                String messageFromClient = new String(datagramPacket.getData(), datagramPacket.getOffset(), datagramPacket.getLength()); // 2param - offset for reading from start
-                String msg;
+                receivedMessages();
 
-                if (messageFromClient.equals("END")){
-                    msg = "Current session is stopped";
-                    System.out.println("END");
-                    //break;
-                } else {
-                    messageFromClient = messageFromClient.replaceAll(".$", "");
-                    String clientName = UDPServer.getClientInputParams(messageFromClient, "\\b\\w+");
-                    String clientDish = UDPServer.getClientInputParams(messageFromClient, "\\b\\w+$");
-                    System.out.print("Message from client: " + messageFromClient + "\nClient name: " +
-                            clientName + "\nClient dish: " + clientDish + "\n");
-                    msg = UDPServer.dishPresentsInMenu(clientName, clientDish);
-                }
-                buffer = msg.getBytes();
-                DatagramPacket response = new DatagramPacket(buffer, buffer.length, inetAddress, port);
-                datagramSocket.send(response);
             } catch (IOException e) {
                 e.printStackTrace();
                 break;
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
+
         }
     }
 
-    public static void main(String[] args) throws SocketException {
+    public static void main(String[] args) throws SocketException, SQLException {
         DatagramSocket datagramSocket = new DatagramSocket(1234);
         UDPServer server = new UDPServer(datagramSocket);
         server.receiveThenSend();
+
+//        String[] arr1 = {"2", "aaa", "2109"};
+//        String[] arr3 = {"1", "ccc", "333"};
+//        String[] arr2 = {"2", "bbb", "333"};
+//        String[] arr4 = {"3", "dda~", "333"};
+//        server.insertInMatrix(arr1);
+//        server.insertInMatrix(arr3);
+//        server.insertInMatrix(arr2);
+//        server.insertInMatrix(arr4);
+//        if (server.ifTilda()) {
+////            if (server.checkLength()) {
+////                server.stingProcessing();   // протестировать
+////            }
+//        }
     }
+
+
+
+
 
 }
